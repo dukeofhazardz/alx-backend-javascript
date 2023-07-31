af6d2849be808c890c07786a17e1af4ba28faf48
Nnaemeka Daniel John <113605239+dukeofhazardz@users.noreply.github.com>
2023-07-31 22:13:34 +0100

    Attempted advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,4 +1,11 @@
 export const weakMap = new WeakMap();
-export default function queryAPI(endpoint) {
 
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
